2f79ae1c31995e2006d5dcf416c34a5e23bd02c7
juancinfante <juaninfantejj@gmail.com>
2023-07-31 00:51:31 -0300

    Añadiendo estilos a pagina admin

--- src/pages/AdminScreen.jsx
+++ src/pages/AdminScreen.jsx
@@ -1,6 +1,24 @@
-
+import '../css/adminScreen.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseUser, faCartShopping, faUser, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 export const AdminScreen = () => {
   return (
-    <div>AdminScreen</div>
+    <>
+    {/* <FontAwesomeIcon icon={faEnvelope} /> */}
+      <div className='admin-container'>
+        <div className='secciones'>
+          <ul>
+            <li><FontAwesomeIcon icon={faUser} /><span>Usuarios</span></li>
+            <li><FontAwesomeIcon icon={faCartShopping} /><span>Productos</span></li>
+            <li><FontAwesomeIcon icon={faPenToSquare} /><span>Pedidos</span></li>
+            <li><FontAwesomeIcon icon={faHouseUser} /><span>Cuenta</span></li>
+          </ul>  
+        </div> 
+        <div>
+          USUARIOS
+        </div> 
+      </div>  
+    
+    </>
   )
 }

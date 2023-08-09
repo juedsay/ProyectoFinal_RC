03419292a1d0a2d2d2afcde1e35d0106478eacb5
Julian Simón <juedsay@gmail.com>
2023-08-09 16:34:50 -0300

    sacando imports

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-solid-svg-icons';
+// import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-solid-svg-icons';
 import '../css/footer.css';
 
 

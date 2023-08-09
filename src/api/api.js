ed9324a9a654baa30b7bcdb7b42bb08d19c880f8
juancinfante <juaninfantejj@gmail.com>
2023-08-09 19:10:52 -0300

    baseURL subida

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4003',
+    baseURL: 'backendproyectofinalrc-production.up.railway.app',
 })
 
 api.interceptors.request.use((config) => {

b969f3d982630a1f55cb1b4a6b9f04444910cd70
juancinfante <juaninfantejj@gmail.com>
2023-08-09 18:21:13 -0300

    .

--- src/pages/ProductScreen.jsx
+++ src/pages/ProductScreen.jsx
@@ -17,38 +17,38 @@ export const ProductScreen = () => {
     imagen: producto.imagen,
     precio: producto.precio,
     cantidad: 1
-}
-const agregarACarrito = (e) => {
-  e.preventDefault();
-  let carritoLocalStorage = JSON.parse(localStorage.getItem('carrito'));
-  if(carritoLocalStorage == null){
+  }
+  const agregarACarrito = (e) => {
+    e.preventDefault();
+    let carritoLocalStorage = JSON.parse(localStorage.getItem('carrito'));
+    if (carritoLocalStorage == null) {
       localStorage.setItem('carrito', JSON.stringify(product));
-      swal("!","Producto añadido.","success");
-  }else{
+      swal("!", "Producto añadido.", "success");
+    } else {
       let productoExite = carritoLocalStorage.filter((prod) => prod.id_prod == product.id_prod);
-      if(productoExite.length !== 0){
-          carritoLocalStorage.forEach(prod => {
-              if(prod.id == product.id){
-                  prod.cantidad += 1;
-              }
-          });
-          localStorage.setItem('carrito',JSON.stringify(carritoLocalStorage));
-          swal("!","Producto añadido.","success");
+      if (productoExite.length !== 0) {
+        carritoLocalStorage.forEach(prod => {
+          if (prod.id == product.id) {
+            prod.cantidad += 1;
+          }
+        });
+        localStorage.setItem('carrito', JSON.stringify(carritoLocalStorage));
+        swal("!", "Producto añadido.", "success");
 
-      }else{
-          carritoLocalStorage.push(product);
-          localStorage.setItem('carrito',JSON.stringify(carritoLocalStorage));
-          swal("!","Producto añadido.","success");
+      } else {
+        carritoLocalStorage.push(product);
+        localStorage.setItem('carrito', JSON.stringify(carritoLocalStorage));
+        swal("!", "Producto añadido.", "success");
 
       }
+    }
   }
-}
   const obtenerProducto = async () => {
     try {
       const producto = await api.get(`/admin/producto/${params.id}`);
       setProducto(producto.data.producto);
     } catch (error) {
-      if(error.response.status == 400){
+      if (error.response.status == 400) {
         navigate('/');
       }
     }
@@ -72,6 +72,17 @@ const agregarACarrito = (e) => {
           <button onClick={(e) => agregarACarrito(e)}>Agregar</button>
         </div>
       </div>
+      {/* <div className='comentarios'>
+        <h4>Comentarios</h4>
+        <div className="comentario">
+          <span className='username'>Juan Cruz: </span>
+          <span className='usercomment'>Muy rico!</span>
+        </div>
+        <form>
+          <input type="text" placeholder="Comentario"/>
+          <button>Enviar</button>
+        </form>
+      </div> */}
     </>
   )
 }

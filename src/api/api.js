a5527d67cfc4771f8d5d7a45a7b3db54740556a4
juancinfante <juaninfantejj@gmail.com>
2023-08-10 20:56:32 -0300

    añadiendo url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4003',
+    baseURL: 'https://backendproyectofinalrc-production.up.railway.app',
 })
 
 api.interceptors.request.use((config) => {
